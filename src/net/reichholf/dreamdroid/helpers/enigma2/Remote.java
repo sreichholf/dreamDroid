1db955f05ed42520b742739a39f54865b1e651c1
sre <stephan@reichholf.net>
2012-09-03 17:34:01 +0200

    * Fix wrong keyid for KEY_PLAY

--- src/net/reichholf/dreamdroid/helpers/enigma2/Remote.java
+++ src/net/reichholf/dreamdroid/helpers/enigma2/Remote.java
@@ -46,7 +46,7 @@ public class Remote extends SimpleResult{
 	public static final int KEY_YELLOW = 400;
 	public static final int KEY_BLUE = 401;
 	public static final int KEY_REWIND = 165;
-	public static final int KEY_PLAY = 164;
+	public static final int KEY_PLAY = 207;
 	public static final int KEY_STOP = 128;
 	public static final int KEY_FORWARD = 163;
 	public static final int KEY_TV = 377;
