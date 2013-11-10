sreichholf/dreamDroid

commit 7c4fb0a4af57e5667983d942dfb198e896fce14d

Fix ActionBar-Progress not showing on android < 3.0

src/net/reichholf/dreamdroid/activities/MainActivity.java
@@ -158,11 +158,14 @@ public class MainActivity extends BaseActivity implements MultiPaneHandler, Prof
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		DreamDroid.setTheme(this);
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		super.onCreate(savedInstanceState);
+
+		supportRequestWindowFeature(Window.FEATURE_PROGRESS);
+		supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
 		getSupportActionBar().setDisplayShowTitleEnabled(true);
 		getSupportActionBar().setDisplayUseLogoEnabled(true);
-		setProgressBarIndeterminateVisibility(false);
+		setSupportProgressBarIndeterminateVisibility(false);
 
 		if (savedInstanceState != null) {
 			mNavigationFragment = (NavigationFragment) getSupportFragmentManager().getFragment(savedInstanceState,
@@ -401,7 +404,7 @@ public class MainActivity extends BaseActivity implements MultiPaneHandler, Prof
 	private void setConnectionState(String state, boolean finished) {
 		mConnectionState.setText(state);
 		if (finished)
-			setProgressBarIndeterminateVisibility(false);
+			setSupportProgressBarIndeterminateVisibility(false);
 	}
 
 	/*

src/net/reichholf/dreamdroid/activities/SimpleFragmentActivity.java
@@ -45,9 +45,11 @@ public class SimpleFragmentActivity extends BaseActivity implements MultiPaneHan
 		if (!mThemeSet)
 			DreamDroid.setTheme(this);
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
-		setProgressBarIndeterminateVisibility(false);
+		supportRequestWindowFeature(Window.FEATURE_PROGRESS);
+		supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
+		setSupportProgressBarIndeterminateVisibility(false);
 		if (getSupportActionBar() != null)
 			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 

src/net/reichholf/dreamdroid/activities/SimpleNoTitleFragmentActivity.java
@@ -11,6 +11,7 @@ import net.reichholf.dreamdroid.R;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Window;
 import android.view.WindowManager;
 
 /**
@@ -21,6 +22,7 @@ public class SimpleNoTitleFragmentActivity extends SimpleFragmentActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		super.onCreate(savedInstanceState);
 	}

src/net/reichholf/dreamdroid/fragment/MediaPlayerFragment.java
@@ -170,7 +170,7 @@ public class MediaPlayerFragment extends AbstractHttpListFragment implements Act
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getActionBarActivity().setProgressBarIndeterminateVisibility(false);
+		getActionBarActivity().setSupportProgressBarIndeterminateVisibility(false);
 		setHasOptionsMenu(true);
 		initTitle(getString(R.string.mediaplayer));
 
@@ -242,7 +242,7 @@ public class MediaPlayerFragment extends AbstractHttpListFragment implements Act
 	public void onLoadFinished(Loader<LoaderResult<ArrayList<ExtendedHashMap>>> loader,
 							   LoaderResult<ArrayList<ExtendedHashMap>> result) {
 
-		getActionBarActivity().setProgressBarIndeterminateVisibility(false);
+		getActionBarActivity().setSupportProgressBarIndeterminateVisibility(false);
 
 		if (loader.getId() == LOADER_PLAYLIST_ID) {
 			setCurrentTitle(getLoadFinishedTitle());

src/net/reichholf/dreamdroid/fragment/ScreenShotFragment.java
@@ -183,7 +183,7 @@ public class ScreenShotFragment extends DreamDroidFragment implements
 		mRawImage = bytes;
 		mImageView.setImageBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
 		mAttacher.update();
-		getActionBarActivity().setProgressBarIndeterminateVisibility(false);
+		getActionBarActivity().setSupportProgressBarIndeterminateVisibility(false);
 	}
 
 	protected void reload() {
@@ -278,14 +278,14 @@ public class ScreenShotFragment extends DreamDroidFragment implements
 
 	@Override
 	public Loader<LoaderResult<byte[]>> onCreateLoader(int id, Bundle args) {
-		getActionBarActivity().setProgressBarIndeterminateVisibility(true);
+		getActionBarActivity().setSupportProgressBarIndeterminateVisibility(true);
 		AsyncByteLoader loader = new AsyncByteLoader(getActionBarActivity(), args);
 		return loader;
 	}
 
 	@Override
 	public void onLoadFinished(Loader<LoaderResult<byte[]>> loader, LoaderResult<byte[]> result) {
-		getActionBarActivity().setProgressBarIndeterminateVisibility(false);
+		getActionBarActivity().setSupportProgressBarIndeterminateVisibility(false);
 		if (!result.isError()) {
 			if (result.getResult().length > 0)
 				onScreenshotAvailable(result.getResult());
@@ -298,7 +298,7 @@ public class ScreenShotFragment extends DreamDroidFragment implements
 
 	@Override
 	public void onLoaderReset(Loader<LoaderResult<byte[]>> loader) {
-		getActionBarActivity().setProgressBarIndeterminateVisibility(false);
+		getActionBarActivity().setSupportProgressBarIndeterminateVisibility(false);
 	}
 
 	protected void showToast(String toastText) {

src/net/reichholf/dreamdroid/fragment/ServiceListFragment.java
@@ -805,7 +805,7 @@ public class ServiceListFragment extends AbstractHttpFragment implements ActionD
 		if (mDetailHeader != null)
 			finishProgress(getString(R.string.services));
 		else
-			getActionBarActivity().setProgressBarIndeterminateVisibility(false);
+			getActionBarActivity().setSupportProgressBarIndeterminateVisibility(false);
 		if (isBouquetList) {
 			mNavItems.clear();
 			mNavItems.addAll(list);

src/net/reichholf/dreamdroid/fragment/ZapFragment.java
@@ -209,7 +209,7 @@ public class ZapFragment extends AbstractHttpListFragment {
 	public void onLoadFinished(Loader<LoaderResult<ArrayList<ExtendedHashMap>>> loader,
 							   LoaderResult<ArrayList<ExtendedHashMap>> result) {
 
-		getActionBarActivity().setProgressBarIndeterminateVisibility(false);
+		getActionBarActivity().setSupportProgressBarIndeterminateVisibility(false);
 		if(mGetBouquetListTask == null){
 			mGetBouquetListTask = new GetBouquetListTask();
 			mGetBouquetListTask.execute();

src/net/reichholf/dreamdroid/fragment/abs/AbstractHttpFragment.java
@@ -164,7 +164,7 @@ public abstract class AbstractHttpFragment extends DreamDroidFragment implements
 
 	@Override
 	public void onLoadFinished(Loader<LoaderResult<ExtendedHashMap>> loader, LoaderResult<ExtendedHashMap> result) {
-		getActionBarActivity().setProgressBarIndeterminateVisibility(false);
+		getActionBarActivity().setSupportProgressBarIndeterminateVisibility(false);
 		setCurrentTitle(getLoadFinishedTitle());
 		getActionBarActivity().setTitle(getCurrentTitle());
 		if (result.isError()) {

commit 78dad7bd30904e7b7c2390c4c6b69f9b4fa7f74f

fix visual glitch introduced with 7c4fb0a4af57e5667983d942dfb198e896fce14d

src/net/reichholf/dreamdroid/activities/MainActivity.java
@@ -159,8 +159,6 @@ public class MainActivity extends BaseActivity implements MultiPaneHandler, Prof
 	public void onCreate(Bundle savedInstanceState) {
 		DreamDroid.setTheme(this);
 		super.onCreate(savedInstanceState);
-
-		supportRequestWindowFeature(Window.FEATURE_PROGRESS);
 		supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
 		getSupportActionBar().setDisplayShowTitleEnabled(true);

src/net/reichholf/dreamdroid/activities/SimpleFragmentActivity.java
@@ -45,8 +45,6 @@ public class SimpleFragmentActivity extends BaseActivity implements MultiPaneHan
 		if (!mThemeSet)
 			DreamDroid.setTheme(this);
 		super.onCreate(savedInstanceState);
-
-		supportRequestWindowFeature(Window.FEATURE_PROGRESS);
 		supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
 		if (getSupportActionBar() != null){