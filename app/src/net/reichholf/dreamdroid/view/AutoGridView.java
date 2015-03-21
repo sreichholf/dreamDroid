a0cabc0e22f81b649dcebff82731ef8dc47abf24
reichi <stephan@reichholf.net>
2015-03-21 20:03:17 +0100

    AutoGridView: import cleanup

--- app/src/net/reichholf/dreamdroid/view/AutoGridView.java
+++ app/src/net/reichholf/dreamdroid/view/AutoGridView.java
@@ -7,7 +7,6 @@ package net.reichholf.dreamdroid.view;
 
 
 import android.content.Context;
-import android.content.res.Configuration;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
