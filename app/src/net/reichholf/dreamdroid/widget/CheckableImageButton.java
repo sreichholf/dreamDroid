ee1793754421ae8b92dd55b32f70bf9d1a33f6cf
reichi <stephan@reichholf.net>
2015-04-06 14:53:03 +0200

    CheckableImageButton: add tooltip handler

--- app/src/net/reichholf/dreamdroid/widget/CheckableImageButton.java
+++ app/src/net/reichholf/dreamdroid/widget/CheckableImageButton.java
@@ -1,10 +1,16 @@
 package net.reichholf.dreamdroid.widget;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Checkable;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by Stephan on 16.11.2014.
@@ -16,18 +22,71 @@ public class CheckableImageButton extends ImageButton implements Checkable {
 
 	public CheckableImageButton(Context context) {
 		super(context);
+		init();
 	}
 
 	public CheckableImageButton(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init();
 	}
 
 	public CheckableImageButton(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		init();
 	}
 
 	public CheckableImageButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
 		super(context, attrs, defStyleAttr, defStyleRes);
+		init();
+	}
+
+	public void init(){
+		setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View view) {
+				CheckableImageButton.this.onLongClick(view, Gravity.BOTTOM);
+				return true;
+			}
+		});
+	}
+
+	public void onLongClick(View view){
+		onLongClick(view, Gravity.TOP);
+	}
+
+	public void onLongClick(View view, int primaryAlign){
+		int secondaryAlign = primaryAlign == Gravity.TOP ? Gravity.BOTTOM : Gravity.TOP;
+
+
+		CharSequence desc = view.getContentDescription();
+		if (desc == null) {
+			// Don't show the cheat sheet for items that already show text.
+			return;
+		}
+
+		final int[] screenPos = new int[2];
+		final Rect displayFrame = new Rect();
+		getLocationOnScreen(screenPos);
+		getWindowVisibleDisplayFrame(displayFrame);
+
+		final Context context = getContext();
+		final int width = getWidth();
+		final int height = getHeight();
+		final int midy = screenPos[1] + height / 2;
+		int referenceX = screenPos[0] + width / 2;
+		if (ViewCompat.getLayoutDirection(view) == ViewCompat.LAYOUT_DIRECTION_LTR) {
+			final int screenWidth = context.getResources().getDisplayMetrics().widthPixels;
+			referenceX = screenWidth - referenceX; // mirror
+		}
+		Toast cheatSheet = Toast.makeText(context, view.getContentDescription(), Toast.LENGTH_SHORT);
+		if (midy < displayFrame.height()) {
+			// Show along the top; follow action buttons
+			cheatSheet.setGravity(primaryAlign| GravityCompat.END, referenceX, height);
+		} else {
+			// Show along the bottom center
+			cheatSheet.setGravity(secondaryAlign| Gravity.CENTER_HORIZONTAL, 0, height);
+		}
+		cheatSheet.show();
 	}
 
 	@Override
