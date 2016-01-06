f7ad707bfdfc42e4a02e058c67a57f0f07669c30
reichi <stephan@reichholf.net>
2016-01-06 21:28:15 +0100

    Strip invalid characters before parsing xml (fixes issues with older webinterfaces)

--- app/src/net/reichholf/dreamdroid/parsers/GenericSaxParser.java
+++ app/src/net/reichholf/dreamdroid/parsers/GenericSaxParser.java
@@ -61,6 +61,25 @@ public class GenericSaxParser implements DataParser {
 		return mHandler;
 	}
 
+
+	protected String stripNonValidXMLCharacters(String in) {
+		StringBuffer out = new StringBuffer(); // Used to hold the output.
+		char current; // Used to reference the current character.
+
+		if (in == null || ("".equals(in))) return ""; // vacancy test.
+		for (int i = 0; i < in.length(); i++) {
+			current = in.charAt(i); // NOTE: No IndexOutOfBoundsException caught here; it should not happen.
+			if ((current == 0x9) ||
+					(current == 0xA) ||
+					(current == 0xD) ||
+					((current >= 0x20) && (current <= 0xD7FF)) ||
+					((current >= 0xE000) && (current <= 0xFFFD)) ||
+					((current >= 0x10000) && (current <= 0x10FFFF)))
+				out.append(current);
+		}
+		return out.toString();
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -71,6 +90,7 @@ public class GenericSaxParser implements DataParser {
 	@Override
 	public boolean parse(String input) {
 		try {
+			input = stripNonValidXMLCharacters(input);
 			mError = false;
 			mErrorText = null;
 			// InputSource is = new InputSource(input);
@@ -81,6 +101,7 @@ public class GenericSaxParser implements DataParser {
 			SAXParserFactory spf = SAXParserFactory.newInstance();
 			spf.setValidating(false);
 			SAXParser sp = spf.newSAXParser();
+
 
 			/* Get the XMLReader of the SAXParser we created. */
 			XMLReader xr = sp.getXMLReader();
