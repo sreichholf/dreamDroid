b2036f8092a61e35072976db332952370ce7309b
reichi <stephan@reichholf.net>
2016-02-02 19:55:45 +0100

    cleanup

--- app/src/net/reichholf/dreamdroid/fragment/EpgBouquetFragment.java
+++ app/src/net/reichholf/dreamdroid/fragment/EpgBouquetFragment.java
@@ -2,7 +2,6 @@ package net.reichholf.dreamdroid.fragment;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.Loader;
 import android.util.Log;
