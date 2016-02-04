de22db11273996fed899b60f885242a22d728cf2
reichi <stephan@reichholf.net>
2016-02-04 07:04:14 +0100

    Add "AutoFitRecyclerView"

---
+++ app/src/net/reichholf/dreamdroid/widget/AutofitRecyclerView.java
@@ -0,0 +1,48 @@
+package net.reichholf.dreamdroid.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+public class AutofitRecyclerView extends RecyclerView {
+	private int mColumnWidth = -1;
+	private int mSpanCount = 4;
+
+	public AutofitRecyclerView(Context context) {
+		super(context);
+		init(context, null);
+	}
+
+	public AutofitRecyclerView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(context, attrs);
+	}
+
+	public AutofitRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(context, attrs);
+	}
+
+	private void init(Context context, AttributeSet attrs) {
+		if (attrs != null) {
+			int[] attrsArray = {
+					android.R.attr.columnWidth
+			};
+			TypedArray array = context.obtainStyledAttributes(attrs, attrsArray);
+			mColumnWidth = array.getDimensionPixelSize(0, -1);
+			array.recycle();
+		}
+	}
+
+	@Override
+	protected void onMeasure(int widthSpec, int heightSpec) {
+		super.onMeasure(widthSpec, heightSpec);
+		if (mColumnWidth > 0) {
+			int newSpanCount = Math.max(1, getMeasuredWidth() / mColumnWidth);
+			mSpanCount = newSpanCount;
+			((GridLayoutManager)getLayoutManager()).setSpanCount(mSpanCount);
+		}
+	}
+}
