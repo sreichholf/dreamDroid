311ff4ece497abada713c5219edd25e1882d3594
reichi <stephan@reichholf.net>
2016-11-13 14:27:14 +0100

    Fix summary handling. Toggle port, too when toggling ssl
    but only if standarf port was set

--- app/src/net/reichholf/dreamdroid/tv/fragment/ProfileFragment.java
+++ app/src/net/reichholf/dreamdroid/tv/fragment/ProfileFragment.java
@@ -2,8 +2,8 @@ package net.reichholf.dreamdroid.tv.fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v17.preference.LeanbackPreferenceFragment;
+import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.Preference;
 
@@ -28,19 +28,38 @@ public class ProfileFragment extends LeanbackPreferenceFragment {
 	@Override
 	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 		setPreferencesFromResource(R.xml.profile_preferences, rootKey);
-		for(String key : sKeys ) {
-			final Preference pref = findPreference(key);
+		for (String key : sKeys) {
+			Preference pref = findPreference(key);
 			pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
 				@Override
 				public boolean onPreferenceChange(Preference preference, Object newValue) {
-					if(pref instanceof EditTextPreference)
-						pref.setSummary((String) newValue);
+					if (DatabaseHelper.KEY_PROFILE_SSL.equals(preference.getKey())) {
+						EditTextPreference portPref = (EditTextPreference) findPreference(DatabaseHelper.KEY_PROFILE_PORT);
+						String condition = "80";
+						String newVal = "443";
+						if (!(Boolean) newValue) {
+							condition = "443";
+							newVal = "80";
+						}
+						if (portPref.getText().equals(condition)) {
+							portPref.setText(newVal);
+							updateSummary(portPref, newVal);
+						}
+					}
+					updateSummary(preference, newValue);
 					return true;
 				}
 			});
-			if(pref instanceof EditTextPreference && !pref.getKey().equals(DatabaseHelper.KEY_PROFILE_PASS))
-				pref.setSummary(((EditTextPreference)pref).getText());
+			updateSummary(pref, null);
 		}
 		getActivity().setResult(Activity.RESULT_OK);
+	}
+
+	protected void updateSummary(Preference pref, Object newValue) {
+		if (pref instanceof EditTextPreference && !pref.getKey().equals(DatabaseHelper.KEY_PROFILE_PASS)) {
+			if (newValue == null)
+				newValue = ((EditTextPreference) pref).getText();
+			pref.setSummary((String) newValue);
+		}
 	}
 }
