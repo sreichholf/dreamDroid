0c6c4b3918480bb301b3b60322a3893a00ac5c2c
reichi <stephan@reichholf.net>
2017-01-15 10:07:54 +0100

    MovieDetailDialog: migrate to BottomSheetDialog

--- app/src/net/reichholf/dreamdroid/fragment/dialogs/MovieDetailDialog.java
+++ app/src/net/reichholf/dreamdroid/fragment/dialogs/MovieDetailDialog.java
@@ -2,9 +2,12 @@ package net.reichholf.dreamdroid.fragment.dialogs;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-
-import com.afollestad.materialdialogs.MaterialDialog;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import net.reichholf.dreamdroid.R;
 import net.reichholf.dreamdroid.helpers.ExtendedHashMap;
@@ -13,10 +16,10 @@ import net.reichholf.dreamdroid.helpers.enigma2.Movie;
 /**
  * Created by Stephan on 05.05.2015.
  */
-public class MovieDetailDialog extends AbstractDialog {
+public class MovieDetailDialog extends BottomSheetActionDialog {
 	private static String KEY_MOVIE = "movie";
 
-	public static MovieDetailDialog newInstance(ExtendedHashMap movie){
+	public static MovieDetailDialog newInstance(ExtendedHashMap movie) {
 		MovieDetailDialog fragment = new MovieDetailDialog();
 		Bundle args = new Bundle();
 		args.putParcelable(KEY_MOVIE, movie);
@@ -25,17 +28,47 @@ public class MovieDetailDialog extends AbstractDialog {
 	}
 
 
-	@NonNull
 	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		ExtendedHashMap movie = getArguments().getParcelable(KEY_MOVIE);
-		MaterialDialog.Builder builder = new MaterialDialog.Builder(getActivity());
-		Dialog dialog = builder
-			.title(movie.getString(Movie.KEY_TITLE))
-			.content(movie.getString(Movie.KEY_DESCRIPTION_EXTENDED))
-			.positiveText(getString(R.string.ok))
-			.build();
+	public void setupDialog(Dialog dialog, int style) {
+		super.setupDialog(dialog, style);
+		View view = LayoutInflater.from(getContext()).inflate(R.layout.movie_epg_dialog, null);
+		dialog.setContentView(view);
 
-		return dialog;
+		ExtendedHashMap movie = getArguments().getParcelable(KEY_MOVIE);
+		String title = movie.getString(Movie.KEY_TITLE, "");
+		String servicename = movie.getString(Movie.KEY_SERVICE_NAME, "");
+		String descShort = movie.getString(Movie.KEY_DESCRIPTION, "");
+		String descEx = movie.getString(Movie.KEY_DESCRIPTION_EXTENDED, "");
+		String length = movie.getString(Movie.KEY_LENGTH, "");
+
+		Toolbar tb = (Toolbar) dialog.findViewById(R.id.toolbar_epg_detail);
+		tb.setTitle(title);
+
+		TextView textServiceName = (TextView) dialog.findViewById(R.id.service_name);
+		if ("".equals(servicename))
+			textServiceName.setVisibility(View.GONE);
+		else
+			textServiceName.setText(servicename);
+
+		TextView textShort = (TextView) dialog.findViewById(R.id.epg_short);
+		if ("".equals(descShort))
+			textShort.setVisibility(View.GONE);
+		else
+			textShort.setText(descShort);
+
+		TextView textLength = (TextView) dialog.findViewById(R.id.movie_length);
+		if ("".equals(length))
+			textLength.setVisibility(View.GONE);
+		else
+			textLength.setText(length);
+
+		TextView textDescEx = (TextView) dialog.findViewById(R.id.epg_description_extended);
+		textDescEx.setText(descEx);
+
+		FrameLayout bottomSheet = (FrameLayout) dialog.findViewById(android.support.design.R.id.design_bottom_sheet);
+		BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+		if (bottomSheetBehavior != null) {
+			bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+		}
 	}
 }
