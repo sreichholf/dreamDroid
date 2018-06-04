7585020a431b95f2d07debb566522970d71ec4e3
reichi <stephan@reichholf.net>
2018-06-04 23:35:42 +0200

    Enable fastscrolling for recycler views using com.simplecityapps:recyclerview-fastscroll

--- app/src/net/reichholf/dreamdroid/widget/AutofitRecyclerView.java
+++ app/src/net/reichholf/dreamdroid/widget/AutofitRecyclerView.java
@@ -3,10 +3,9 @@ package net.reichholf.dreamdroid.widget;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 
-public class AutofitRecyclerView extends RecyclerView {
+public class AutofitRecyclerView extends com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView {
 	public static int DEFAULT_MAX_SPAN_COUNT = -1;
 	private int mColumnWidth = -1;
 	private int mSpanCount = 4;
