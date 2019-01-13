e5bbd1c9f3627e43b67fb48fc343b5f8807217e0
reichi <stephan@reichholf.net>
2019-01-13 11:43:30 +0100

    ProfileList: properly cancel device search task onPause, set mProgress to NOT-cancelable

--- app/src/net/reichholf/dreamdroid/fragment/ProfileListFragment.java
+++ app/src/net/reichholf/dreamdroid/fragment/ProfileListFragment.java
@@ -145,6 +145,7 @@ public class ProfileListFragment extends BaseRecyclerFragment implements DetectD
 					.content(R.string.searching_known_devices)
 					.cancelable(false);
 			mProgress = IndeterminateProgress.newInstance(R.string.searching, R.string.searching_known_devices);
+			mProgress.setCancelable(false);
 			getMultiPaneHandler().showDialogFragment(mProgress, "dialog_devicesearch_indeterminate");
 			mDetectDevicesTask = new DetectDevicesTask(this);
 			mDetectDevicesTask.execute();
@@ -212,6 +213,7 @@ public class ProfileListFragment extends BaseRecyclerFragment implements DetectD
 		builder.show();
 	}
 
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		mCardListStyle = true;
 		mHasFabMain = true;
@@ -247,6 +249,19 @@ public class ProfileListFragment extends BaseRecyclerFragment implements DetectD
 		reloadProfiles();
 	}
 
+	@Override
+	public void onPause() {
+		if (mDetectDevicesTask != null) {
+			mDetectDevicesTask.cancel(true);
+			mDetectDevicesTask = null;
+		}
+		IndeterminateProgress progress = (IndeterminateProgress) getFragmentManager().findFragmentByTag("dialog_devicesearch_indeterminate");
+		if (progress != null) {
+			progress.dismiss();
+		}
+		mProgress = null;
+		super.onPause();
+	}
 
 	@Override
 	public void onItemClick(RecyclerView parent, View view, int position, long id) {
