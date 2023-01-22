bd840abb3ef9c38b99472482cce18bd3eeecde0b
reichi <stephan@reichholf.net>
2023-01-22 10:43:23 +0100

    VideoOverlay: don't call isInPictureInPictureMode on API < 24

--- app/src/net/reichholf/dreamdroid/fragment/VideoOverlayFragment.java
+++ app/src/net/reichholf/dreamdroid/fragment/VideoOverlayFragment.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.DisplayMetrics;
@@ -747,7 +748,7 @@ public class VideoOverlayFragment extends Fragment implements MediaPlayer.EventL
 	}
 
 	public void showOverlays() {
-		if (getActivity().isInPictureInPictureMode()) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && getActivity().isInPictureInPictureMode()) {
 			hideOverlays();
 			return;
 		}
